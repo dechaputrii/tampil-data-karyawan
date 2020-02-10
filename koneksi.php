afaced360a649820ce7e991221e25e13696a548d
dechaputrii <33824654+dechaputrii@users.noreply.github.com>
2020-02-10 17:21:08 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-// konfigurasi database
-$host       =   "localhost";
-$user       =   "root";
-$password   =   "";
-$database   =   "marketplace";
-
-$koneksi = mysqli_connect($host, $user, $password, $database);
-?>
